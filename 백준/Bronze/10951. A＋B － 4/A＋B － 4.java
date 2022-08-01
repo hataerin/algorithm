3e6261bb34e70ec724a5ecd05cd8ae1d136dc3ba
hataerin <goil1113@likelion.org>
2022-08-01 17:05:05 +0900

    [Bronze V] Title: A+B - 4, Time: 240 ms, Memory: 17952 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNext()) {
+        while (sc.hasNext()) { //입력된 토큰이 있으면 true, 없으면 false
             int A = sc.nextInt();
             int B = sc.nextInt();
 
