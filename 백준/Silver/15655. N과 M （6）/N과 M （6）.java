98ec9875a726bc11f32e3d5150b0c69e12d86fc3
hataerin <goil1113@likelion.org>
2023-02-08 16:31:47 +0900

    [Silver III] Title: N과 M (6), Time: 148 ms, Memory: 15984 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,23 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-
     static int N, M;
-    static int[] element, numbers;
+    static int[] elements, numbers;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        element = new int[N];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        elements = new int[N];
         numbers = new int[M];
 
-        for (int i = 0; i < N; i++) {
-            element[i] = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        for (int n = 0; n < N; n++) {
+            elements[n] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(element);
+        Arrays.sort(elements);
 
         combination(0, 0);
         System.out.println(sb);
@@ -27,20 +32,17 @@ public class Main {
     private static void combination(int cnt, int start) {
         //기저조건
         if (cnt == M) {
-            print(numbers);
+            for (int i = 0; i < M; i++) {
+                sb.append(numbers[i] + " ");
+            }
+            sb.append("\n");
             return;
         }
+
         //유도파트
         for (int i = start; i < N; i++) {
-            numbers[cnt] = element[i];
-            combination(cnt+1, i+1);
+            numbers[cnt] = elements[i];
+            combination(cnt + 1, i + 1);
         }
-    }
-
-    private static void print(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            sb.append(arr[i] + " ");
-        }
-        sb.append("\n");
     }
 }
