f949f28fee1ef48cf325e85511ce3e3d06f7c114
hataerin <goil1113@likelion.org>
2023-02-08 15:02:47 +0900

    [Silver III] Title: N과 M (3), Time: 664 ms, Memory: 212368 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,19 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
     static int N, M;
-    static int[] numbers; //결과값 저장
+    static int[] numbers;
+    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
         numbers = new int[M];
 
         permutation(0);
@@ -24,21 +19,17 @@ public class Main {
     private static void permutation(int cnt) {
         //기저조건
         if (cnt == M) {
-            print(numbers);
+            for (int i = 0; i < M; i++) {
+                sb.append(numbers[i] + " ");
+            }
+            sb.append("\n");
             return;
         }
+
         //유도파트
         for (int i = 1; i <= N; i++) {
             numbers[cnt] = i;
-            permutation(cnt+1);
+            permutation(cnt + 1);
         }
-    }
-
-
-    private static void print(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            sb.append(arr[i] + " ");
-        }
-        sb.append("\n");
     }
 }
