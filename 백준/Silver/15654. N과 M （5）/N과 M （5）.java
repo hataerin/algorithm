d2caf043e040a5d68f5118b55df139ddcaaecb79
hataerin <goil1113@likelion.org>
2023-02-08 16:11:12 +0900

    [Silver III] Title: N과 M (5), Time: 436 ms, Memory: 41644 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,53 +1,53 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
-    static int[] elements;
-    static int[] numbers;
+    static int[] elements, numbers;
     static boolean[] isSelected;
-
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        N = sc.nextInt();
-        M = sc.nextInt();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         elements = new int[N];
-        numbers = new int[M];
         isSelected = new boolean[N];
+        numbers = new int[M];
 
-        for (int i = 0; i < N; i++) {
-            elements[i] = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        for (int n = 0; n < N; n++) {
+            elements[n] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(elements);
 
         permutation(0);
+        System.out.println(sb);
     }
 
     //중복X 순열
     private static void permutation(int cnt) {
         //기저조건
         if (cnt == M) {
-            print(numbers);
+            for (int i = 0; i < M; i++) {
+                sb.append(numbers[i] + " ");
+            }
+            sb.append("\n");
             return;
         }
 
         //유도파트
         for (int i = 0; i < N; i++) {
-            if(isSelected[i]) continue;
+            if (isSelected[i]) continue;
             numbers[cnt] = elements[i];
             isSelected[i] = true;
             permutation(cnt + 1);
             isSelected[i] = false;
         }
-    }
-
-    private static void print(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
     }
 }
