9805913dd1cc2fa7442e18c53668e7f47581f3ef
hataerin <goil1113@likelion.org>
2023-02-14 16:15:12 +0900

    [D2] Title: 파리 퇴치, Time: 159 ms, Memory: 24,656 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,45 +1,46 @@
 import java.util.Scanner;
-  
+
 public class Solution {
-  
-    static int T, N, M;
-  
+    static int T, N, M, sum, max;
+    static int[][] map;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-  
+
         T = sc.nextInt();
-  
-        for (int t = 1; t <= T; t++) { //테케만큼 반복
-  
+        for (int t = 1; t <= T; t++) {
             N = sc.nextInt();
             M = sc.nextInt();
-  
-            int arr[][] = new int[N][N];
-  
-            for (int r = 0; r < N; r++) { // 파리 개수 입력
-                for (int c = 0; c < N; c++) {
-                    arr[r][c] = sc.nextInt();
+            map = new int[N][N];
+
+            //입력
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    map[i][j] = sc.nextInt();
                 }
             }
-  
-            int max = 0;
-            for (int r = 0; r < N; r++) {
-                for (int c = 0; c < N; c++) { //완탐
-  
-                    int sum = 0;
-                    for (int i = 0; i < M; i++) {
-                        for (int j = 0; j < M; j++) { //파리채 크기
-  
-                            if (r + i >= 0 && r + i < N && c + j >= 0 && c + j < N) { //범위체크
-                                sum += arr[r + i][c + j]; // 죽은 파리의 개수
-                            }
-                        }
-                    }
-                    if (sum > max) max = sum; //파리 최댓값 갱신
+
+            //구현
+            max = -1;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    sum = 0;
+                    trap(i, j);
                 }
             }
-            System.out.println("#"+t+" "+max); // 최댓값 출력력
-  
+
+            //출력
+            System.out.println("#" + t + " " + max);
         }
-   }
+    }
+
+    private static void trap(int x, int y) {
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < M; j++) {
+                if (x+i < 0 || x+i >= N || y+j < 0 || y+j >= N) continue;
+                sum += map[x + i][y + j];
+            }
+        }
+        if (max < sum) max = sum;
+    }
 }
