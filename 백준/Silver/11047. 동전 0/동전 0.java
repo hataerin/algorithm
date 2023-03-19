eed8d2da0293f4896347efb1957306ff8e95b1d3
hataerin <goil1113@likelion.org>
2023-03-20 01:04:21 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,27 +1,40 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K, cnt;
-    static int[] coin;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.nextInt();
+	static int N;
+	static int K;
+	static int[] value;
+	
+	static int ans = 0;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		value = new int[N];
+		
+		for(int i = 0 ; i < N ; i++) {
+			value[i] = Integer.parseInt(br.readLine());
+		}
+		
+		
+		for(int i = N -1 ; i >= 0 ; i--) {
+			if(K / value[i] >= 1) {
+				ans += K/value[i];
+				K = K%value[i];
+			}
+		}
+		
+		System.out.println(ans);
+		
+		}
 
-        coin = new int[N];
-        for (int n = 0; n < N; n++) {
-            coin[n] = sc.nextInt();
-        }
-
-        cnt = 0;
-        for (int n = N-1; n >= 0; n--) {
-            if (n < 0 || K < coin[n]) continue;
-            while (K >= coin[n]) {
-                K -= coin[n];
-                cnt++;
-            }
-        }
-        System.out.println(cnt);
-    }
 }
